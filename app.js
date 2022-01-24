8691aa826d38d8fd8d1c0220f3e994efeeac808f
Kady <ku.ayush@yahoo.com>
2022-01-24 18:40:18 +0530

    Aa

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.post('/failure', function (req,res) {
     res.redirect('/');
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 5000, function () {
     console.log("Server is running");
 });
